cc4504ad5a5c700f9c8883fb23ae1cd8be24894a
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-09-04 22:36:01 +0800

    Install autotable

--- reports/src/app/app.module.ts
+++ reports/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VisitorReportComponent } from './visitor-report/visitor-report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisitorReportComponent
   ],
   imports: [
     BrowserModule,
