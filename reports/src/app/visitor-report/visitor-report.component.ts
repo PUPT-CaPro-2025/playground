fbd9b8faef51f58d17de055a6cbfff6dcc60f300
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-09-04 22:54:03 +0800

    create sample reports

--- reports/src/app/visitor-report/visitor-report.component.ts
+++ reports/src/app/visitor-report/visitor-report.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-visitor-report',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class VisitorReportComponent {
 
+	generatePDF() {
+		// Create a new PDF document.
+		const doc = new jsPDF();
+
+		// Add content to the PDF.
+		doc.setFontSize(16);
+		doc.text('My Angular PDF Generator', 10, 10);
+		doc.setFontSize(12);
+		doc.text(
+			'This is a comprehensive guide on generating PDFs with Angular.',
+			10,
+			20,
+		);
+
+		// Create a table using `jspdf-autotable`.
+		const headers = [['Name', 'Email', 'Country']];
+		const data = [
+			['David', 'david@example.com', 'Sweden'],
+			['Castille', 'castille@example.com', 'Spain'],
+			// ...
+		];
+
+		autoTable(doc, {
+			head: headers,
+			body: data,
+			startY: 30, // Adjust the `startY` position as needed.
+		});
+
+		// Save the PDF.
+		doc.save('table.pdf');
+	}
 }
