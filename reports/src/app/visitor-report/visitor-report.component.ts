b235dfa31f3cce0659267176fea54361a388a4f3
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-09-04 22:56:37 +0800

    feat: Open generated PDF in a new window instead of saving it

--- reports/src/app/visitor-report/visitor-report.component.ts
+++ reports/src/app/visitor-report/visitor-report.component.ts
@@ -5,39 +5,43 @@ import autoTable from 'jspdf-autotable';
 @Component({
   selector: 'app-visitor-report',
   templateUrl: './visitor-report.component.html',
-  styleUrl: './visitor-report.component.css'
+  styleUrls: ['./visitor-report.component.css']
 })
 export class VisitorReportComponent {
 
-	generatePDF() {
-		// Create a new PDF document.
-		const doc = new jsPDF();
+  generatePDF() {
+    // Create a new PDF document.
+    const doc = new jsPDF();
 
-		// Add content to the PDF.
-		doc.setFontSize(16);
-		doc.text('My Angular PDF Generator', 10, 10);
-		doc.setFontSize(12);
-		doc.text(
-			'This is a comprehensive guide on generating PDFs with Angular.',
-			10,
-			20,
-		);
+    // Add content to the PDF.
+    doc.setFontSize(16);
+    doc.text('My Angular PDF Generator', 10, 10);
+    doc.setFontSize(12);
+    doc.text(
+      'This is a comprehensive guide on generating PDFs with Angular.',
+      10,
+      20,
+    );
 
-		// Create a table using `jspdf-autotable`.
-		const headers = [['Name', 'Email', 'Country']];
-		const data = [
-			['David', 'david@example.com', 'Sweden'],
-			['Castille', 'castille@example.com', 'Spain'],
-			// ...
-		];
+    // Create a table using `jspdf-autotable`.
+    const headers = [['Name', 'Email', 'Country']];
+    const data = [
+      ['David', 'david@example.com', 'Sweden'],
+      ['Castille', 'castille@example.com', 'Spain'],
+      // Add more data as needed
+    ];
 
-		autoTable(doc, {
-			head: headers,
-			body: data,
-			startY: 30, // Adjust the `startY` position as needed.
-		});
+    autoTable(doc, {
+      head: headers,
+      body: data,
+      startY: 30, // Adjust the `startY` position as needed.
+    });
 
-		// Save the PDF.
-		doc.save('table.pdf');
-	}
+    // Get the PDF data as a Blob.
+    const pdfBlob = doc.output('blob');
+
+    // Create a URL for the Blob and open it in a new window.
+    const pdfUrl = URL.createObjectURL(pdfBlob);
+    window.open(pdfUrl);
+  }
 }
